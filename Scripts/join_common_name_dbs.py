97a798d4709477ccae825850fb6407e46703494b
snair09 <sandeep.j.nair@gmail.com>
2023-08-04 13:31:58 -0700

    change path to match re org

--- Scripts/join_common_name_dbs.py
+++ Scripts/join_common_name_dbs.py
@@ -1,12 +1,12 @@
 import sys
 import pandas as pd
 
-#output = pd.read_csv('../test/test_data/prefetch_trial.csv')
-output_path = sys.argv[1]
-output = pd.read_csv(output_path)
-final_FooDB = pd.read_csv('../Common-name/final_FooDB.csv')
-final_trnl =pd.read_csv('../Common-name/final_trnl.csv')
-final_USDA = pd.read_csv('../Common-name/final_USDA.csv')
+#results = pd.read_csv('../test/test_data/prefetch_trial.csv')
+input_path = sys.argv[1]
+results = pd.read_csv(input_path)
+final_FooDB = pd.read_csv('../db/Common-name-cleaned/final_FooDB.csv')
+final_trnl =pd.read_csv('../db/Common-name-cleaned/final_trnl.csv')
+final_USDA = pd.read_csv('../db/Common-name-cleaned/final_USDA.csv')
 
 def std_name(name):
     word_list = name.split()
@@ -18,15 +18,16 @@ def std_name(name):
     return sci_name
 
 #split match_name
-output['scientific_name'] = output['match_name'].apply(lambda x: pd.Series(std_name(x)))
+#results['scientific_name'] = results['match_name'].apply(lambda x: pd.Series(std_name(x))) #For prefetch
+results['scientific_name'] = results['name'].apply(lambda x: pd.Series(std_name(x)))
 
 #joins
-output = output.merge(final_USDA, on='scientific_name',how='left')
-output = output.merge(final_trnl, on='scientific_name',how='left')
-output = output.merge(final_FooDB, on='scientific_name',how='left')
+results = results.merge(final_USDA, on='scientific_name',how='left')
+results = results.merge(final_trnl, on='scientific_name',how='left')
+results = results.merge(final_FooDB, on='scientific_name',how='left')
 
 #print summary
 
-num_rows_with_na_all_cols = output[['usda_common_name', 'foodb_common_name', 'trnl_common_name']].isna().all(axis=1).sum()
-print(f'Out of {output.shape[0]} matches,{output.shape[0] - num_rows_with_na_all_cols} were annotated with common names')
+num_rows_with_na_all_cols = results[['usda_common_name', 'foodb_common_name', 'trnl_common_name']].isna().all(axis=1).sum()
+print(f'Out of {results.shape[0]} matches,{results.shape[0] - num_rows_with_na_all_cols} were annotated with common names')
 print(sys.argv[1])
