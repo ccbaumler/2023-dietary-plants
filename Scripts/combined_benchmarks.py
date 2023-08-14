8df8d47d0eb8fc40f55418825f26ddc6d61e24a8
ccbaumler <ccbaumler@ucdavis.edu>
2023-08-14 15:28:27 -0700

    adding benchmark tsv processing script

--- Scripts/combined_benchmarks.py
+++ Scripts/combined_benchmarks.py
@@ -1,32 +1,53 @@
 import os
-
-def calculate_average(file_path, value_column_index):
-    with open(file_path, 'r') as f:
-        lines = f.readlines()
-
-    values = [ float(line.strip().split('\t')[value_column_index]) for line in lines[1:]]
-    average = sum(values) / len(values)
-
-    print(values)
-    return average
+import pandas as pd
 
 def main():
-    input_directory = "../snakemake-workflow/benchmarks"  # Update this to the directory containing your benchmark files
-    output_file = "averages.tsv"  # Update this to the desired output file name
-    value_column_index = 3  # Update this to the index of the column containing the values
+    input_directory = "../snakemake-workflow/benchmarks"
+    output_file = "benchmark.report.tsv"
 
-    averages = {}
+    combo = "all.benchmarks.tsv"
 
-    for filename in os.listdir(input_directory):
-        if filename.endswith(".tsv"):
-            file_path = os.path.join(input_directory, filename)
-            file_name_without_extension = os.path.splitext(filename)[0]
-            average = calculate_average(file_path, value_column_index)
-            averages[file_name_without_extension] = average
+    combine_tsvs(input_directory, combo) 
 
-    with open(output_file, 'w') as f:
-        for file_name, average in sorted(averages.items()):
-            f.write(f"{file_name}\t{average}\n")
+    grouped_data = calculate_values(combo)
+    grouped_data.to_csv(output_file, sep='\t')
+
+def calculate_values(fp):
+    df = pd.read_csv(fp, sep="\t")
+    df = df.drop(columns=["h:m:s"])
+    
+    agg_columns = ["s", "max_rss"]#, "max_vms", "max_uss", "max_pss", "mean_load", "cpu_time"]
+    agg_functions = ["mean", "max", "min"]
+    grouped = df.groupby("filename")[agg_columns].agg(agg_functions)
+    grouped.columns = [f"{col}_{agg}" for col in grouped.columns.levels[0] for agg in grouped.columns.levels[1]]
+    
+    return grouped
+
+    group = df.groupby("filename").agg(
+            s_mean=pd.NamedAgg(column="s", aggfunc='mean'),
+            s_min=pd.NamedAgg(column="s", aggfunc='min'),
+            s_max=pd.NamedAgg(column="s", aggfunc='max'),
+            max_rss_mean=pd.NamedAgg(column="max_rss", aggfunc='mean'),
+            max_rss_min=pd.NamedAgg(column="max_rss", aggfunc='min'),
+            max_rss_max=pd.NamedAgg(column="max_rss", aggfunc='max'),
+            )          
+    return group    
+
+def combine_tsvs(input_dir, output):
+    #try os.walk and fn
+    #ls_filenames = [ x for x in glob('*.{}'.format('tsv')) ] #This gives a list of all files in working dir
+    for root, dirs, files in os.walk(f'{input_dir}'):
+        ls_filenames= [ str.rsplit(file, sep='.', maxsplit=1)[0] for file in files]
+    
+    dataframes = []
+
+    for fp in sorted(ls_filenames):
+        df = pd.read_csv(input_dir + "/" + fp + ".tsv", sep='\t')
+        df['filename'] = fp
+        dataframes.append(df)
+
+    combined_tsv = pd.concat(dataframes)
+    combined_tsv.to_csv(output, sep='\t', index=False)
 
 if __name__ == "__main__":
     main()
