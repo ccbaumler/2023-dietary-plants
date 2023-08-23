210d85d2fe097bf0ff380b3e444dd8c28859aa2c
Sandeep Nair <sandeep.j.nair@gmail.com>
2023-08-23 13:55:56 -0700

    updated snakefile and reorg output files/directories

--- snakemake-workflow/snake_slurm.sh
+++ snakemake-workflow/snake_slurm.sh
@@ -1,15 +1,15 @@
 #! /bin/bash -login
 #SBATCH -p bmh
-#SBATCH -J plant_dna
-#SBATCH -t 5-10:00:00
+#SBATCH -J chr_adolescent
+#SBATCH -t 1-5:00:00
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -c 8
+#SBATCH -c 10
 #SBATCH --mem=60gb
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=sjnair@ucdavis.edu
-#SBATCH -o /home/sjnair/2022-benchmark/sourmash-gather/output/reports/slurm-%j.out
-#SBATCH -e /home/sjnair/2022-benchmark/sourmash-gather/output/reports/slurm-%j.err
+#SBATCH -o /home/sjnair/reports/slurm-%j.out
+#SBATCH -e /home/sjnair/reports/slurm-%j.err
 
 # activate conda
 . "/home/sjnair/miniconda3/etc/profile.d/conda.sh"
@@ -18,4 +18,4 @@
 conda activate snakemake
 
 # run snakefile 
-snakemake -s Snakefile -c 8 --rerun-incomplete -k
+snakemake -s Snakefile -c 10 --rerun-incomplete -k --config group="adolescent"
