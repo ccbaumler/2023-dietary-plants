0dfde8f7e05f71d5086c06c57a7629232aabfdfa
Sandeep Nair <sandeep.j.nair@gmail.com>
2023-08-31 13:52:09 -0700

    removed output files from tracking

--- snakemake-workflow/smk_adolesc.sh
+++ snakemake-workflow/smk_adolesc.sh
@@ -1,11 +1,11 @@
 #! /bin/bash -login
-#SBATCH -p bmh
-#SBATCH -J chr_adolescent
+#SBATCH -p bmm
+#SBATCH -J chr_adult-2
 #SBATCH -t 20:00:00
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -c 10
-#SBATCH --mem=50gb
+#SBATCH --mem=30gb
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=sjnair@ucdavis.edu
 #SBATCH -o /home/sjnair/reports/slurm-%j.out
@@ -19,4 +19,4 @@ conda activate snakemake
 
 # run snakefile 
 snakemake --unlock
-snakemake -s Snakefile -c 10 -k --config group="adolescent"
+snakemake -s Snakefile -c 10 -k --config group="adult-2"
